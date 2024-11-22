8a617c8d8b8ccefa2e6bbe299c330bef8be1127a
nguyenhoang887 <dtc21h4801030123@ictu.edu.vn>
2024-11-22 09:48:05 +0700

    first commit

---
+++ server/src/services/auth.js
@@ -0,0 +1,48 @@
+import db from '../models'
+import bcrypt from 'bcryptjs'
+import { raw } from 'express'
+import jwt from 'jsonwebtoken'
+import {v4} from 'uuid'
+require('dotenv').config()
+
+const hashPassword = password => bcrypt.hashSync(password, bcrypt.genSaltSync(12))
+
+export const registerService = ({phone, password, name}) => new Promise(async(resole, reject) => {
+    try {
+        const response = await db.User.findOrCreate({
+            where: {phone},
+            defaults: {
+                phone,
+                name, 
+                password: hashPassword(password), 
+                id: v4()
+            }
+        })
+        const token = response[1] && jwt.sign({id: response[0].id, phone: response[0].phone}, process.env.SECRET_KEY, {expiresIn: '2d'})
+        resole({
+            err: token ? 0 : 2,
+            msg: token ? 'Register is succesfully !' : 'Phone number has been aldready used !',
+            token: token || null
+        })
+    } catch (error) {
+        reject(error)
+    }
+})
+
+export const loginService = ({phone, password}) => new Promise(async(resole, reject) => {
+    try {
+        const response = await db.User.findOne({
+            where: {phone},
+            raw: true
+        })
+        const isCorrectPassword = response && bcrypt.compareSync(password, response.password)
+        const token = isCorrectPassword && jwt.sign({id: response.id, phone: response.phone}, process.env.SECRET_KEY, {expiresIn: '2d'})
+        resole({
+            err: token ? 0 : 2,
+            msg: token ? 'Login is succesfully !' : response ? 'Password is wrong !' : 'Phone number not found !',
+            token: token || null
+        })
+    } catch (error) {
+        reject(error)
+    }
+})
